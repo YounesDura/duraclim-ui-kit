f98cdd9d7a5f5e40a73e4d0217a5335fb52d8fa0
Younes Drissi <younes.d@duraclim.com>
2025-04-16 21:14:08 +0100

    input changes

--- src/components/Inputs/Inputs.tsx
+++ src/components/Inputs/Inputs.tsx
@@ -16,7 +16,6 @@ interface InputProps extends Omit<React.InputHTMLAttributes<HTMLInputElement | H
   value?: string;
   onChange?: (value: string, isValid: boolean) => void;
   customValidation?: (value: string) => boolean;
-  isSearchable?: boolean;
 }
 
 const Input = ({
@@ -28,46 +27,35 @@ const Input = ({
   onChange,
   customValidation,
   className,
-  isSearchable,
   ...props
 }: InputProps) => {
   const [isFocused, setIsFocused] = useState(false);
   const [isValid, setIsValid] = useState(true);
   const [errorMessage, setErrorMessage] = useState('');
-  const [isOpen, setIsOpen] = useState(false);
-  const [searchTerm, setSearchTerm] = useState('');
 
-  const validateInput = (value: string): boolean => {
-    if (!value) return true;
+  const validateInput = (val: string): boolean => {
+    if (!val) return true;
 
     let isValidValue = true;
     let message = '';
 
     switch (type) {
       case 'email':
-        isValidValue = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
+        isValidValue = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(val);
         message = 'Please enter a valid email address';
         break;
-      case 'tel':
-        isValidValue = /^[\d\s+()-]{10,}$/.test(value);
-        message = 'Please enter a valid phone number';
-        break;
-      case 'number':
-        isValidValue = !isNaN(Number(value));
-        message = 'Please enter a valid number';
-        break;
       case 'url':
-        isValidValue = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/.test(value);
+        isValidValue = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/.test(val);
         message = 'Please enter a valid URL';
         break;
       case 'date':
-        isValidValue = !isNaN(Date.parse(value));
+        isValidValue = !isNaN(Date.parse(val));
         message = 'Please enter a valid date';
         break;
     }
 
     if (customValidation) {
-      isValidValue = isValidValue && customValidation(value);
+      isValidValue = isValidValue && customValidation(val);
       message = 'Invalid input';
     }
 
@@ -92,65 +80,39 @@ const Input = ({
     ${className || ''}
   `.trim();
 
-  const handleSelectClick = () => {
-    setIsOpen(!isOpen);
-  };
-
-  const handleOptionSelect = (optionValue: string) => {
-    onChange?.(optionValue, true);
-    setIsOpen(false);
-    setSearchTerm('');
-  };
-
-  const filteredOptions = options?.filter(option => 
-    option.label.toLowerCase().includes(searchTerm.toLowerCase())
-  );
-
   return (
     <div className={combinedClasses}>
+      {icon && <div className={styles.icon}>{icon}</div>}
+
       {type === 'select' ? (
-        <div className={styles.selectWrapper}>
-          <div 
-            className={styles.input}
-            onClick={handleSelectClick}
-          >
-            {icon && <div className={styles.icon}>{icon}</div>}
-            <span className={styles.selectText}>
-              {value ? options?.find(opt => opt.value === value)?.label : placeholder}
-            </span>
-          </div>
-          
-          {isOpen && (
-            <div className={styles.dropdown}>
-              <div className={styles.optionsList}>
-                {filteredOptions?.map((opt) => (
-                  <div
-                    key={opt.value}
-                    className={styles.option}
-                    onClick={() => handleOptionSelect(opt.value)}
-                  >
-                    {opt.label}
-                  </div>
-                ))}
-              </div>
-            </div>
-          )}
-        </div>
+        <select
+          className={styles.input}
+          value={value}
+          onChange={handleChange}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          {...props}
+        >
+          {placeholder && <option value="">{placeholder}</option>}
+          {options?.map((opt) => (
+            <option key={opt.value} value={opt.value}>
+              {opt.label}
+            </option>
+          ))}
+        </select>
       ) : (
-        <>
-          {icon && <div className={styles.icon}>{icon}</div>}
-          <input
-            type={type}
-            className={styles.input}
-            placeholder={placeholder}
-            value={value}
-            onChange={handleChange}
-            onFocus={handleFocus}
-            onBlur={handleBlur}
-            {...props}
-          />
-        </>
+        <input
+          type={type}
+          className={styles.input}
+          placeholder={placeholder}
+          value={value}
+          onChange={handleChange}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          {...props}
+        />
       )}
+
       {!isValid && errorMessage && (
         <div className={styles.errorMessage}>{errorMessage}</div>
       )}
